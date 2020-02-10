fd41d65426aa70e7cb86a2fa802b413afcb38a8a
hoang <ngpbhoang1406@gmail.com>
2020-02-10 23:16:33 +0700

    Remove leaves project, leaving root OCL2PSQL

--- src/main/webapp/tools/ssl2plsql/blockly/generators/javascript/action.js
+++
@@ -1,7 +0,0 @@
-Blockly.JavaScript['add'] = function(block) {
-    var value_name = Blockly.JavaScript.valueToCode(block, 'NAME', Blockly.JavaScript.ORDER_ATOMIC);
-    // TODO: Assemble JavaScript into code variable.
-    var code = '...';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.JavaScript.ORDER_NONE];
-  };
