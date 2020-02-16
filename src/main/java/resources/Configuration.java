25c3ef51504e40428888ae030088332fa5e81218
hoang <ngpbhoang1406@gmail.com>
2020-02-16 22:18:21 +0700

    Update Configuration.java

--- src/main/java/resources/Configuration.java
+++ src/main/java/resources/Configuration.java
@@ -4,21 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.sql.DataSource;
 
 public class Configuration {
 
     public static Connection getConnection()
         throws SQLException, NamingException {
 
-        Context initContext = new InitialContext();
+//        Context initContext = new InitialContext();
         // This is very specific to Tomcat
-        Context envContext = (Context) initContext.lookup("java:comp/env");
+//        Context envContext = (Context) initContext.lookup("java:comp/env");
 
-        DataSource ds = (DataSource) envContext.lookup("jdbc/ttcdb");
+//        DataSource ds = (DataSource) envContext.lookup("jdbc/ttcdb");
 //        return ds.getConnection();
         
         return getRemoteConnection();
@@ -27,7 +24,7 @@ public class Configuration {
     private static Connection getRemoteConnection() {
         if (System.getProperty("RDS_HOSTNAME") != null) {
             try {
-                Class.forName("org.postgresql.Driver");
+                Class.forName("com.mysql.jdbc.Driver");
                 String dbName = System.getProperty("RDS_DB_NAME");
                 String userName = System.getProperty("RDS_USERNAME");
                 String password = System.getProperty("RDS_PASSWORD");
